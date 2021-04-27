15bcd51697f99180a49d163ccf5d9f069215342f
unknown <jhonjlinaresb@gmail.com>
2021-04-27 20:32:34 +0200

    Creación de promesas async await en las rutas

--- routes/api/dvd.js
+++ routes/api/dvd.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('¡Server Funciona Correctamente!');
+const { Dvd } = require('../../db');
+
+router.get('/', async (req, res) => {
+    const dvds = await Dvd.findAll();
+    res.json(dvds);
+});
+
+router.post('/', async (req, res) => {
+    const dvd = await Dvd.create(req.body);
+    res.json(dvd);
 });
 
 module.exports = router;
