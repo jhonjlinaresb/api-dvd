eb799f338280348c7eece913835141f3bf4a4a86
unknown <jhonjlinaresb@gmail.com>
2021-04-28 18:48:18 +0200

    Incorporación de Promesa para buscar por id

--- routes/api/dvd.js
+++ routes/api/dvd.js
@@ -12,6 +12,17 @@ router.post('/', async (req, res) => {
     res.json(dvd);
 });
 
+router.get('/:dvdId', async (req, res) => {
+    const viewDvd = await Dvd.findAll( req.body, { 
+        where: { id: req.params.dvdId} });
+    res.json(viewDvd);
+});
+
+/* router.get('/:id', async (req, res) => {
+    const viewDvd = await Dvd.find( { id: req.params.id} );
+    res.json(viewDvd);
+}); */
+
 router.put('/:dvdId', async (req, res) => {
     await Dvd.update(req.body, {
         where: { id: req.params.dvdId }
