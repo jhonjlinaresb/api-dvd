b497dc05520129f6511ec68ef17e287c167a21cb
unknown <jhonjlinaresb@gmail.com>
2021-05-07 23:38:04 +0200

    Agregar rutas para usuarios en la api

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const apiDvdRouter = require('./api/dvd');
+const apiUsersRouter = require('./api/users');
 
 router.use('/dvd', apiDvdRouter);
+router.use('/users', apiUsersRouter);
 
 module.exports = router;
