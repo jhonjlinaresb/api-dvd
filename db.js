8eb8d587d2005f374b8b960dda66de5c42155b0c
unknown <jhonjlinaresb@gmail.com>
2021-05-08 00:10:46 +0200

    Creación de usuario en API con clave encriptada

--- db.js
+++ db.js
@@ -3,15 +3,15 @@ const Sequelize = require('sequelize');
 const dvdModel = require('./models/dvd');
 const userModel = require('./models/users');
 
-const sequielize = new Sequelize('5GF4HSSXRE', '5GF4HSSXRE', 'Eo2LjCn9J4', {
+const sequelize = new Sequelize('5GF4HSSXRE', '5GF4HSSXRE', 'Eo2LjCn9J4', {
     host: 'remotemysql.com',
     dialect: 'mysql'
 })
 
-const Dvd = dvdModel(sequielize, Sequelize);
-const User = userModel(sequielize, Sequelize);
+const Dvd = dvdModel(sequelize, Sequelize);
+const User = userModel(sequelize, Sequelize);
 
-sequielize.sync({ force: false})
+sequelize.sync({ force: false})
 .then(()=>{
     console.log('Tablas correctamente sincronizadas');
 })
