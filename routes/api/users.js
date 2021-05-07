c6d1a0de07746ea6aba42a3e18979819bed1cf8c
unknown <jhonjlinaresb@gmail.com>
2021-05-08 00:34:28 +0200

    Correción de errores

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../../db');
+const { check, validationResult } = require('express-validator');
 
-router.post('/register', async (req, res) => {
+router.post('/register', [
+    check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio').notEmpty(),
+    check('email', 'Debe ser un Email, es obligatorio').isEmail()
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({ errores: errors.array() })
+    }
 
     req.body.password = bcrypt.hashSync(req.body.password, 10);
     const user = await User.create(req.body);
-    res.json(user);
+    res.json( user);
 
 });
 
