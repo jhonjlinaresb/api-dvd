3d1561498e3264e647e0c1ed6a292442791dbd92
unknown <jhonjlinaresb@gmail.com>
2021-05-07 23:32:44 +0200

    Sincronización a base de datos tabla users

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
 const dvdModel = require('./models/dvd');
+const userModel = require('./models/users');
 
 const sequielize = new Sequelize('5GF4HSSXRE', '5GF4HSSXRE', 'Eo2LjCn9J4', {
     host: 'remotemysql.com',
@@ -8,6 +9,7 @@ const sequielize = new Sequelize('5GF4HSSXRE', '5GF4HSSXRE', 'Eo2LjCn9J4', {
 })
 
 const Dvd = dvdModel(sequielize, Sequelize);
+const User = userModel(sequielize, Sequelize);
 
 sequielize.sync({ force: false})
 .then(()=>{
@@ -15,5 +17,6 @@ sequielize.sync({ force: false})
 })
 
 module.exports = {
-    Dvd
+    Dvd, 
+    User
 }
