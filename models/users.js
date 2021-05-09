22f4d4f474c88b1ded8bd37cd573fa9c53d7d89f
unknown <jhonjlinaresb@gmail.com>
2021-05-10 00:08:17 +0200

    Añadir nuevas librerias para JWT en login

--- models/users.js
+++ models/users.js
@@ -3,8 +3,7 @@ module.exports = (sequelize, type) =>{
         id: {
             type: type.INTEGER,
             primaryKey: true,
-            autoIncrement: true,
-            unique: true
+            autoIncrement: true
         },
         username: type.STRING,
         email: type.STRING, 
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../../db');
 const { check, validationResult } = require('express-validator');
+const jwt = require('jwt-simple');
+const moment = require('moment');
 
 router.post('/register', [
     check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
@@ -19,5 +21,23 @@ router.post('/register', [
     res.json( user);
 
 });
+
+router.post('/login', async (req, res) =>{
+    const user = await User.findOne({ where : { email: req.body.email } });
+    if (user) {
+        const verifyPass = bcrypt.compareSync(req.body.password, user.password);
+        if (verifyPass) {
+            res.json({ success: 'TOKEN'});
+        } else {
+            res.json({ error: 'La contraseña es incorrecta'});
+        }
+    } else {
+        res.json({ error: 'Error en email y/o contraseña'});
+    }
+});
+
+const createToken = (user) => {
+
+}
 
 module.exports = router;
